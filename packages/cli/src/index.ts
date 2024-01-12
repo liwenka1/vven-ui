8f27fb0ef8679e0db2ee726dc527ac7b36a3acee
liwenkai <2020583117@qq.com>
2024-01-12 18:00:30 +0800

    feat: 添加commander

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,13 @@
-console.log('未来可期')
+#! /usr/bin/env node
+
+import { Command } from 'commander'
+
+const main = () => {
+  const program = new Command()
+
+  program.name('vven-ui').description('来给你的组件添加一些好用的组件').version('0.0.1')
+
+  program.parse()
+}
+
+main()
