58137be64e6222f6fab4bee66a5cc2715a3bc3dc
liwenkai <2020583117@qq.com>
2024-01-14 01:20:55 +0800

    fix: 解决格式化无效问题

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -5,7 +5,15 @@ import { Command } from 'commander'
 const main = () => {
   const program = new Command()
 
-  program.name('vven-ui').description('来给你的组件添加一些好用的组件').version('0.0.1')
+  program
+    .name('vven-ui')
+    .description('来给你的组件添加一些好用的组件')
+    .version('0.0.1')
+    .option('-f, --force', 'overwrite target directory if it exist')
+    .action((name, options) => {
+      // 打印执行结果
+      console.log('name:', name, 'options:', options)
+    })
 
   program.parse()
 }
