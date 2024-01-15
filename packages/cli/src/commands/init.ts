55a8a4c1b5e0573af55ea24f7124656b3dec0d10
liwenkai <2020583117@qq.com>
2024-01-15 18:03:30 +0800

    feat: 添加简单写入文件的功能

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,10 +1,47 @@
 import { Command } from 'commander'
+import prompts from 'prompts'
+import chalk from 'chalk'
+import fse from 'fs-extra'
+import path from 'path'
+
+import { rawConfig } from '@/utils/zod'
 
 const program = new Command()
+
+const promptForConfig = async () => {
+  const highlight = (text: string) => chalk.cyan(text)
+
+  const options = await prompts([
+    {
+      type: 'toggle',
+      name: 'typescript',
+      message: `Would you like to use ${highlight('TypeScript')} (recommended)?`,
+      initial: true,
+      active: 'yes',
+      inactive: 'no'
+    }
+  ])
+
+  const config = rawConfig.parse({
+    typescript: options.typescript
+  })
+
+  const cwd = process.cwd()
+  const targetAir = path.join(cwd, 'components.json')
+  if (fse.existsSync(targetAir)) {
+    console.log(config, cwd)
+  } else {
+    await fse.writeFile(targetAir, JSON.stringify(config, null, 2))
+  }
+
+  return options
+}
 
 const init = program
   .name('init')
   .description('初始化脚手架配置')
-  .action((name) => console.log('init', name))
+  .action(() => {
+    promptForConfig()
+  })
 
 export default init
--- packages/cli/src/utils/figlet.ts
+++ packages/cli/src/utils/figlet.ts
@@ -4,7 +4,7 @@ export const helpFiglet = () => {
   // 使用 figlet 绘制 Logo
   console.log(
     '\r\n' +
-      figlet.textSync('zhurong', {
+      figlet.textSync('vven-ui', {
         font: 'Ghost',
         horizontalLayout: 'default',
         verticalLayout: 'default',
